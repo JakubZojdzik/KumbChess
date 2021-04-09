d27a5d95492362068fc479ed56f2bfe43db6929b
Jakub <jakub.zojdzik@gmail.com>
2021-04-09 13:45:04 +0200

    Board and Piece classes, displaying board

---
+++ main.py
@@ -0,0 +1,95 @@
+import sys
+import pygame
+from pygame.locals import *
+from piece import Piece
+from piece import Board
+
+pygame.init()
+
+fps = 60
+fpsClock = pygame.time.Clock()
+
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+
+# Game variables
+board = []  # p - pawn, r - rook, n - knight, b - bishop, q - queen, k - king, 0 - white, 1 - black
+board_sprite = Board()
+all_sprites = pygame.sprite.Group()
+
+
+def pos_to_cords(x, y):
+    return 176 + 64 * y, 76 + 64 * x
+
+
+def resetBoard():
+    global board
+    board.clear()
+    board = [[], [], [], [], [], [], [], []]
+    for i in range(8):
+        for j in range(8):
+            board[i].append('')
+
+    # Pawns
+    for i in range(8):
+        board[1][i] = Piece('p1')
+        board[6][i] = Piece('p0')
+
+    # Rocks
+    board[0][0] = Piece('r1')
+    board[0][7] = Piece('r1')
+    board[7][0] = Piece('r0')
+    board[7][7] = Piece('r0')
+
+    # Knights
+    board[0][1] = Piece('n1')
+    board[0][6] = Piece('n1')
+    board[7][1] = Piece('n0')
+    board[7][6] = Piece('n0')
+
+    # Bishops
+    board[0][2] = Piece('b1')
+    board[0][5] = Piece('b1')
+    board[7][2] = Piece('b0')
+    board[7][5] = Piece('b0')
+
+    # Queens
+    board[0][3] = Piece('q1')
+    board[7][3] = Piece('q0')
+
+    # Kings
+    board[0][4] = Piece('k1')
+    board[7][4] = Piece('k0')
+
+
+def draw_board():
+    global all_sprites
+    global board
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(board_sprite)
+    for i in range(8):
+        for j in range(8):
+            if board[i][j] != '':
+                board[i][j].rect = board[i][j].image.get_rect(center=pos_to_cords(i, j))
+                all_sprites.add(board[i][j])
+
+    all_sprites.update()
+    all_sprites.draw(screen)
+
+
+resetBoard()
+# Game loop.:
+while True:
+    screen.fill((209, 170, 111))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Update.
+    draw_board()
+    # Draw.
+    all_sprites.draw(screen)
+    pygame.display.flip()
+    fpsClock.tick(fps)
---
+++ piece.py
@@ -0,0 +1,58 @@
+import pygame
+
+pygame.display.set_mode((800, 600))
+pygame.init()
+p0 = pygame.image.load('img/pawn_white.png').convert_alpha()
+p1 = pygame.image.load('img/pawn_black.png').convert_alpha()
+r0 = pygame.image.load('img/rock_white.png').convert_alpha()
+r1 = pygame.image.load('img/rock_black.png').convert_alpha()
+n0 = pygame.image.load('img/knight_white.png').convert_alpha()
+n1 = pygame.image.load('img/knight_black.png').convert_alpha()
+b0 = pygame.image.load('img/bishop_white.png').convert_alpha()
+b1 = pygame.image.load('img/bishop_black.png').convert_alpha()
+q0 = pygame.image.load('img/queen_white.png').convert_alpha()
+q1 = pygame.image.load('img/queen_black.png').convert_alpha()
+k0 = pygame.image.load('img/king_white.png').convert_alpha()
+k1 = pygame.image.load('img/king_black.png').convert_alpha()
+board_img = pygame.image.load('img/board.png').convert_alpha()
+
+
+class Piece(pygame.sprite.Sprite):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+        pygame.sprite.Sprite.__init__(self)
+        self.image = p0
+        if model == 'p0':
+            self.image = p0
+        elif model == 'p1':
+            self.image = p1
+        elif model == 'r0':
+            self.image = r0
+        elif model == 'r1':
+            self.image = r1
+        elif model == 'n0':
+            self.image = n0
+        elif model == 'n1':
+            self.image = n1
+        elif model == 'b0':
+            self.image = b0
+        elif model == 'b1':
+            self.image = b1
+        elif model == 'q0':
+            self.image = q0
+        elif model == 'q1':
+            self.image = q1
+        elif model == 'k0':
+            self.image = k0
+        elif model == 'k1':
+            self.image = k1
+        self.rect = 0
+
+
+class Board(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        pygame.sprite.Sprite.__init__(self)
+        self.image = board_img
+        self.rect = self.image.get_rect(center=(400, 300))
